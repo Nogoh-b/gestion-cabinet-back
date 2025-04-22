2f3999046e04651c187f56c1594e3b2161402042
freddyley@nkees <freddyleyankees@gmail.com>
2025-04-22 07:38:20 +0200

    setup microservice listen on port 3002

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: { host: '0.0.0.0', port: 3002 },
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen();
 }
 bootstrap();
