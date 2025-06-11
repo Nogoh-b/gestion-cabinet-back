26db359a7b6f2dee043f755b5070cabad096ae9c
Nogoh Brice <nogohbrice@gmail.com>
2025-06-11 18:01:21 +0100

    `Added a line of whitespace before the password property in the AppDataSource configuration.`

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,6 @@
 import { DataSource } from 'typeorm';
 
+
 // ... importe toutes tes entités
 
 export const AppDataSource = new DataSource({
@@ -7,7 +8,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '3306', 10),
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME, 
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
