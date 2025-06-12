86678c0ef7fae66f39098439eaf26af28f96bf15
Nogoh Brice <nogohbrice@gmail.com>
2025-06-12 10:53:50 +0100

    `Update CreateTransactionSavingsAccountDto and TransactionSavingsAccountService to add optional properties`

--- src/modules/transaction/transaction_saving_account/dto/create-transaction_saving_account.dto.ts
+++ src/modules/transaction/transaction_saving_account/dto/create-transaction_saving_account.dto.ts
@@ -11,6 +11,7 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 
 
+
 export class CreateTransactionSavingsAccountDto {
   @ApiProperty({ example: 1000.00, description: 'Montant de la transaction' })
   @IsNumber()
@@ -66,7 +67,7 @@ export class CreateCreditTransactionSavingsAccountDto {
   amount: number;
   @ApiPropertyOptional({ example: '8629891', description: 'Code de destination' })
   @IsString()
-  target_savings_account_code: string;         // code du compte épargne cible (pour INTERNAL_TRANSFER)
+  target_savings_account_code?: string;         // code du compte épargne cible (pour INTERNAL_TRANSFER)
 
   @ApiPropertyOptional({ example: false, description: 'Si la transaction est bloquée ou pas' })
   @IsBoolean()
--- src/modules/transaction/transaction_saving_account/transaction_saving_account.service.ts
+++ src/modules/transaction/transaction_saving_account/transaction_saving_account.service.ts
@@ -16,11 +16,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 
 
+
+
 import { ChannelTransaction } from '../chanel-transaction/entities/channel-transaction.entity';
 import { TransactionTypeService } from '../transaction_type/transaction_type.service';
 import { CreateCreditTransactionSavingsAccountDto, CreateDebitTransactionSavingsAccountDto, CreateTransactionSavingsAccountDto, ValidateTransactionSavingsAccountDto } from './dto/create-transaction_saving_account.dto';
 import { Sequence } from './entities/sequence.entity';
 import { TransactionSavingsAccount } from './entities/transaction_saving_account.entity';
+
+
 
 
 
