aaab5714a5fba64ef4dd9fc9125539be2ad1316d
freddy kabong <freddyleyankees@gmail.com>
2025-08-17 19:45:51 +0100

    resolve dependencies errors

--- src/modules/credit/credit.module.ts
+++ src/modules/credit/credit.module.ts
@@ -1,8 +1,34 @@
 import { Module } from '@nestjs/common';
 import { TypeCreditModule } from './type_credit/typeCredit.module';
 import { LoanModule } from './loan/loan.module';
+import { UsersService } from '../iam/user/user.service';
+import { User } from '../iam/user/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from '../customer/customer/entities/customer.entity';
+import { UserRole } from '../iam/user-role/entities/user-role.entity';
+import { UserRolesService } from '../iam/user-role/user-role.service';
+import { RolePermissionService } from '../iam/role-permission/role-permission.service';
+import { RolePermission } from '../iam/role-permission/entities/role-permission.entity';
+import { PermissionsService } from '../iam/permission/permission.service';
+import { Permission } from '../iam/permission/entities/permission.entity';
 
 @Module({
-  imports: [LoanModule, TypeCreditModule],
+  imports: [
+    LoanModule,
+    TypeCreditModule,
+    TypeOrmModule.forFeature([
+      User,
+      Customer,
+      UserRole,
+      RolePermission,
+      Permission,
+    ]),
+  ],
+  providers: [
+    UsersService,
+    UserRolesService,
+    RolePermissionService,
+    PermissionsService,
+  ],
 })
 export class CreditModule {}
--- src/modules/credit/type_credit/typeCredit.module.ts
+++ src/modules/credit/type_credit/typeCredit.module.ts
@@ -7,10 +7,37 @@ import { TypeCreditController } from './typeCredit.controller';
 import { TypeGuarantyController } from '../guaranty/type_guaranty/type_guaranty.controller';
 import { TypeGuarantyService } from '../guaranty/type_guaranty/type_guaranty.service';
 import { TypeGuaranty } from '../guaranty/type_guaranty/entity/type_guaranty.entity';
+import { UsersService } from '../../iam/user/user.service';
+import { UserRolesService } from '../../iam/user-role/user-role.service';
+import { RolePermissionService } from '../../iam/role-permission/role-permission.service';
+import { PermissionsService } from '../../iam/permission/permission.service';
+import { User } from '../../iam/user/entities/user.entity';
+import { Customer } from '../../customer/customer/entities/customer.entity';
+import { UserRole } from '../../iam/user-role/entities/user-role.entity';
+import { RolePermission } from '../../iam/role-permission/entities/role-permission.entity';
+import { Permission } from '../../iam/permission/entities/permission.entity';
 
 @Module({
-  imports: [CoreModule, TypeOrmModule.forFeature([TypeCredit, TypeGuaranty])],
-  providers: [TypeCreditService, TypeGuarantyService],
+  imports: [
+    CoreModule,
+    TypeOrmModule.forFeature([
+      TypeCredit,
+      TypeGuaranty,
+      User,
+      Customer,
+      UserRole,
+      RolePermission,
+      Permission,
+    ]),
+  ],
+  providers: [
+    TypeCreditService,
+    TypeGuarantyService,
+    UsersService,
+    UserRolesService,
+    RolePermissionService,
+    PermissionsService,
+  ],
   controllers: [TypeCreditController, TypeGuarantyController],
 })
 export class TypeCreditModule {}
