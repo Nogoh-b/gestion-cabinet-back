b3b83c54959ba86f0f96a3d289ea1c6c38a5b6b7
freddy kabong <freddyleyankees@gmail.com>
2025-08-20 17:21:37 +0100

    add upload

--- src/modules/credit/loan/dto/loan.dto.ts
+++ src/modules/credit/loan/dto/loan.dto.ts
@@ -41,14 +41,24 @@ export class LoanDto {
   reference: string;
 }
 
-export class DocumentsLoanDto {
-  @ApiProperty()
+export class DocumentLoanDto {
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    format: 'binary',
+    example:
+      '{"fieldname":"documents[0][files]","originalname":"create-customer.dto.ts","encoding":"7bit","mimetype":"images;pdf","buffer":{"type":"Buffer","data":[47,47,32,99,114,101,97,116,101,45,99,117,115,116,111,109,101,114,46,100,116,111,46,116,115,13,10,105,109,112,111,114,116,32,123,32,65,112,105,80,114,111,112,101,114,116,121,32,108,101,58,32,49,32,125,41,13,10,32,32,116,121,112,101,95,99,117,115,116,111,109,101,114,95,105,100,58,32,110,117,109,98,101,114,59,13,10,125,13,10]},"size":1660},"customer_id":20}',
+  })
+  file: Express.Multer.File;
+
+  @ApiProperty({
+    required: true,
+  })
   @IsNumber()
-  documentId: number;
+  typeOfDocument: number;
 }
 
-
-export class DocumentLoanDto {
+export class GuarantyDocumentLoanDto extends DocumentLoanDto {
   @ApiProperty({
     required: true,
   })
@@ -60,12 +70,6 @@ export class DocumentLoanDto {
   })
   @IsNumber()
   typeGuaranty: number;
-
-  @ApiProperty({
-    required: true,
-  })
-  @IsNumber()
-  documentId: number;
 }
 
 export class SubmitLoanDto {
