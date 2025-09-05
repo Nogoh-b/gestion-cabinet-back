920732d00eb182c21482fa0f242d784c63b22c0a
Nogoh Brice <nogohbrice@gmail.com>
2025-09-05 05:28:19 +0200

    `Added null check for origin variable and updated conditions in TransactionSavingsAccountService`

--- src/modules/transaction/transaction_saving_account/transaction_saving_account.service.ts
+++ src/modules/transaction/transaction_saving_account/transaction_saving_account.service.ts
@@ -146,6 +146,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 
 
+
 import { ChannelTransaction } from '../chanel-transaction/entities/channel-transaction.entity';
 import {
   TransactionChannel,
@@ -163,6 +164,7 @@ import {
 import { ResponseTransactionSavingsAccountDto } from './dto/response-transaction_saving_account.dto';
 import { Sequence } from './entities/sequence.entity';
 import { Payment, PaymentStatus, PaymentStatusProvider, TransactionSavingsAccount, TransactionSavingsAccountStatus } from './entities/transaction_saving_account.entity';
+
 
 
 
@@ -437,7 +439,7 @@ export class TransactionSavingsAccountService {
     txType.code === TransactionCode.INTERNAL_TRANSFER  ){    */
     // this.validate(tx.id, isFirstTx)
     // console.log('txxxxxxxx11 ', tx.targetSavingsAccount ,' ', tx.status)
-    if (
+    if (origin != null ||
       (provider.code != TransactionProvider.MOMO &&
         provider.code != TransactionProvider.OM) ||
         txType.code === TransactionCode.BUY_TONTINE ||
