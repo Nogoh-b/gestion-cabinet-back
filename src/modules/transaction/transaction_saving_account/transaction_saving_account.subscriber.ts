6b8a59e95efb4569191d7689a6f0588f418598b1
Nogoh Brice <nogohbrice@gmail.com>
2025-09-17 00:36:47 +0200

    `Refactor transaction_saving_account.subscriber.ts: update imports and modify afterInsert method`

--- src/modules/transaction/transaction_saving_account/transaction_saving_account.subscriber.ts
+++ src/modules/transaction/transaction_saving_account/transaction_saving_account.subscriber.ts
@@ -8,7 +8,7 @@ import {
 import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { TransactionSavingsAccountService } from 'src/modules/transaction/transaction_saving_account/transaction_saving_account.service';
-import { TransactionSavingsAccount } from './entities/transaction_saving_account.entity';
+import { TransactionProvider, TransactionSavingsAccount } from './entities/transaction_saving_account.entity';
 import { SavingsAccountService } from 'src/modules/savings-account/savings-account/savings-account.service';
 
 @EventSubscriber()
@@ -29,14 +29,20 @@ export class TransactionSavingsAccountSubscriber implements EntitySubscriberInte
   }
 
   async afterInsert(event: InsertEvent<TransactionSavingsAccount>) {
-    console.log('afterInsert111 ', event.entity.id);
-    
-      /*this.transactionSavingsAccountService.repeatEvery(5, 3, async () => {
-        console.log('Tâche exécutée à', event.entity.payment_code);
-        const tx = await this.transactionSavingsAccountService.checkWthDraw(event.entity.payment_code);
-        console.log('Found via service:', tx);
-      });
-      setImmediate(async () => {
+      const tx = event.entity
+        console.log('afterInsert111 ', tx.provider_code === TransactionProvider.OM);
+        if((tx.provider_code === TransactionProvider.MOMO || tx.provider_code === TransactionProvider.OM) && !tx.has_issue ){
+          this.transactionSavingsAccountService.repeatEvery(15, 3, async () => {
+                console.log('Tâche exécutée à', event.entity.channelTransaction.code , ' ', event.entity.originSavingsAccount ,  ' ',event.entity.targetSavingsAccount);
+                try {
+                  const tx1 = await this.transactionSavingsAccountService.checkWthDraw(event.entity.reference);
+                  console.log('Found via service:', tx1);  
+                } catch (error) {
+                  
+                }
+          });
+        }
+      /*setImmediate(async () => {
         try {
         } catch (e) {
           console.error('Erreur service après insert:', e);
