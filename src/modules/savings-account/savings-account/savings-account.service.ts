b9962edc470022dda95308d7e0855efe095ab9b9
freddy kabong <freddyleyankees@gmail.com>
2025-09-22 14:39:53 +0100

    configuration de la recuperation de compte avec la variable all

--- src/modules/savings-account/savings-account/savings-account.service.ts
+++ src/modules/savings-account/savings-account/savings-account.service.ts
@@ -308,7 +308,7 @@ export class SavingsAccountService extends BaseService<SavingsAccount> {
     return account;
   }
 
-  async findOneByCode(number_savings_account: string, all = true): Promise<SavingsAccountResponseDto | SavingsAccount> {
+  async findOneByCode(number_savings_account: string, all = true): Promise<SavingsAccountResponseDto | SavingsAccount | any> {
     const relations = [
       'customer',
       'type_savings_account',
@@ -319,20 +319,23 @@ export class SavingsAccountService extends BaseService<SavingsAccount> {
       'interestRelations',
     ];
 
-    // if (all) {
+     if (all) {
       relations.push('originSavingsAccountTx', 'targetSavingsAccountTx'); 
-    // }
+     }
 
 
     const account = await this.repo.findOne({
       where: { number_savings_account , status: Not(SavingsAccountStatus.DEACTIVATE)},
       relations,
     });
-    if (!account) throw new NotFoundException(`Compte ${number_savings_account} introuvable`);
-    const soldes = await this.updateBalance(account.id)
-    account.avalaible_balance = soldes.avalaible_balance
-    account.balance = await soldes.balance
-    account.avalaible_balance_online = await soldes.avalaible_balance_online
+    if (all) {
+      if (!account) throw new NotFoundException(`Compte ${number_savings_account} introuvable`);
+      const soldes = await this.updateBalance(account.id)
+      account.avalaible_balance = soldes.avalaible_balance
+      account.balance = await soldes.balance
+      account.avalaible_balance_online = await soldes.avalaible_balance_online
+    }
+
     return !all ? plainToInstance(SavingsAccountResponseDto, account) : account;
   }
 
